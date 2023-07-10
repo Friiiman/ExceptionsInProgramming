6e503c059a685f52ab1bac2022727ca46a6bdd85
Friiiman <friiiman94@gmail.com>
2023-07-10 19:20:54 +0300

    hw3

---
+++ Modules/InputData.java
@@ -0,0 +1,21 @@
+package HW3.Modules;
+
+import java.util.Scanner;
+
+public class InputData {
+    public String[] enterData() {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите строку через пробел как показанно на примере: " +
+                    "(Петров Пётр Петрович 11.11.1111 1234567890 f или m )");
+            String data = sc.nextLine();
+            String[] arrayData = data.split(" ");
+            if (arrayData.length == 6) {
+                return arrayData;
+            } else if (arrayData.length < 6) {
+                System.out.println("Вы ввели не все данные, попробуйте ещё раз.");
+            } else
+                System.out.println("Вы ввели слишком много данных, попробуйте ещё раз.");
+        }
+    }
+}
